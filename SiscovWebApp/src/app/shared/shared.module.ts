16afa9e3bdbb0424a50592c22f212db1d62a5af1
daniel.gonzaga <daniel.gonzaga@tegra.com.br>
2021-10-13 17:34:34 -0300

    deletion of search-bar component

--- SiscovWebApp/src/app/shared/shared.module.ts
+++ SiscovWebApp/src/app/shared/shared.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { NavbarComponent } from './navbar/navbar.component';
-import { SearchBarComponent } from './search-bar/search-bar.component';
 import { LocalsListComponent } from './locals-list/locals-list.component';
 import { AccordionModule } from 'primeng/accordion';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
@@ -17,7 +16,6 @@ import { CarouselModule } from 'primeng/carousel';
 @NgModule({
   declarations: [
     NavbarComponent,
-    SearchBarComponent,
     LocalsListComponent,
     LocalsListItemComponent,
     LoaderComponent,
@@ -34,7 +32,6 @@ import { CarouselModule } from 'primeng/carousel';
   ],
   exports: [
     NavbarComponent,
-    SearchBarComponent,
     LocalsListComponent,
     LoaderComponent,
     NotFoundComponent,
